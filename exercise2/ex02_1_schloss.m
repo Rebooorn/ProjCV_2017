a25db05f435fc3d07658f02eae53d11a5b47b483
Rebooorn <liuchangzju@gmail.com>
2017-11-16 01:41:54 +0800

    planar rectification finished

--- exercise2/ex02_1_schloss.m
+++ exercise2/ex02_1_schloss.m
@@ -15,14 +15,14 @@ x = readNpoints(P,4);
 x_prime = readNpoints(F,4);
 
 % normalize and transform to homogeneous coordinates
-x_mean = mean(x,2);
-x_prime_mean = mean(x_prime,2);
-s_x = sqrt(2)*mean( sqrt(sum( (x-repmat(x_mean,1,4)).^2,1 )) );
-s_x_prime = sqrt(2)*mean( sqrt(sum( (x_prime-repmat(x_prime_mean,1,4)).^2,1 )) );
-x(1,:)=(x(1,:)-x_mean(1))/s_x;
-x(2,:)=(x(2,:)-x_mean(2))/s_x;
-x_prime(1,:)=(x_prime(1,:)-x_prime_mean(1))/s_x_prime;
-x_prime(2,:)=(x_prime(2,:)-x_prime_mean(2))/s_x_prime;
+% x_mean = mean(x,2);
+% x_prime_mean = mean(x_prime,2);
+% s_x = sqrt(2)*mean( sqrt(sum( (x-repmat(x_mean,1,4)).^2,1 )) );
+% s_x_prime = sqrt(2)*mean( sqrt(sum( (x_prime-repmat(x_prime_mean,1,4)).^2,1 )) );
+% x(1,:)=(x(1,:)-x_mean(1))/s_x;
+% x(2,:)=(x(2,:)-x_mean(2))/s_x;
+% x_prime(1,:)=(x_prime(1,:)-x_prime_mean(1))/s_x_prime;
+% x_prime(2,:)=(x_prime(2,:)-x_prime_mean(2))/s_x_prime;
 
 x=[x;ones(1,size(x,2))];
 x_prime = [x_prime;ones(1,size(x_prime,2))];
@@ -35,7 +35,7 @@ reprojection_error=reprojection_error_homography(x_prime,H,x)
 
 % Warp image and display
 % ...
-tform = maketform('projective',H);
+tform = maketform('projective',H');
 P_mapping = imtransform(P,tform);
 figure;
 hold on
