6b03ecd63901f51c532ea82e175cc82f2604eecb
nidhijoshi123 <nidhi.nj.joshi@fau.de>
2018-01-02 17:48:51 +0100

    Update ex03_2b_computeF.m
    Fundamental matrix finish

--- exercise3/skeleton/ex03_2b_computeF.m
+++ exercise3/skeleton/ex03_2b_computeF.m
@@ -3,9 +3,16 @@ function [ F ] = ex03_2b_computeF( x, x_prime )
 % a set of correpsonding points (8 Point Algorithmn).
 % x1 = [3xn] points in the first image
 % x2 = [3xn] points in the second image
-
 % Returns F such that x_2^T F x = 0;
 
+
+%randomly select 8 point correspondences out of 20 from x and x_prime, 3*8 matrices
+s = randperm(8);
+x=x(:,s(1:8));
+x_prime=x_prime(:,s(1:8));
+
+
+% Set n=8
     n = size(x,2);
 
     % Normalization
@@ -14,16 +21,20 @@ function [ F ] = ex03_2b_computeF( x, x_prime )
 
     % Compose linear matrix A
     % TODO!
-    % A = ???
+   A = [ repmat(x2(1,:)',1,3) .* x1', repmat(x2(2,:)',1,3) .* x1', x1(1:3,:)'];
     
     % Calculate F matrix
-    % TODO!
-    
+   [U,S,V] = svd(A);
+
     % Make it rank 2 
-    % TODO!
+    F_norm = reshape(V(:,end),3,3)';
+
+[uf,sf,vf] = svd(F_norm);
+F_norm_prime = uf*diag([sf(1) sf(5) 0])*(vf');
+
     
     % Revert normalization
     % TODO!
-    % F= ???
+    F= Tx2' * F_norm_prime* Tx1;
 end
 
